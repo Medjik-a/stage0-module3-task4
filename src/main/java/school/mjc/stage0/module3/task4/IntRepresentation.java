90a6c17f3e7ea5aae0e12ce3a02001356fc4941f
medjik <med.akhmetov@gmail.com>
2023-07-01 15:29:59 +0600

    fourth attempt

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,9 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        System.out.println((int)'c');
-        System.out.println((int)'h');
-        System.out.println((int)'a');
-        System.out.println((int)'r');
+        char c=99,h=104,a=97,r=114;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
