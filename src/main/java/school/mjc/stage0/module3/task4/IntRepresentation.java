68d5909b802a12ffe59584ccf29e39199fd6b8a4
medjik <med.akhmetov@gmail.com>
2023-07-01 15:13:44 +0600

    first attempt

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        short a='c',b= 'h',c= 'a', d='r';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
